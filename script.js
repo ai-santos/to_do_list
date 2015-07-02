815d9ab471c4d0b495c16c3726cc7835953ee4c9
aileen santos <asantos3026@gmail.com>
2015-07-01 21:58:45 -0700

    updated to_do_list

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 $(function() {
 
- 
-  var $taskName = $("#taskname").val();
-  var description = $("#description").val();
-  var toDoData = {name: taskname, description: description}
 
-  var $newItemForm = $("#new_task");
-  var tasklist = [];
+ //create new todo object from form data
+  var toDoTask = $("#task-name").val();
+  var toDoDesc = $("#task-desc").val();
+  var toDoData = {name: toDoTask, description: toDoDesc}
 
-  $newItemForm.on("submit", function(event) {
-    event.preventDefault();
-    console.log('form submitted!');
-    console.log($("#taskname").val());
-  });    
+  var newToDoList = $("#new_to_do_list");
+  var newItemForm = $("#new_task");
+  var tasklist = $("#task-list");
 
+var templatingFunction = _.template($('#task-list-template').html());
 
+var toDoItems = [
+  {todo: "Clean Up", description: "vacuum"},
+  {todo: "Read", description: "Underscore Docs"},
+  {todo: "Study", description: "Homework"}
+];
 
-// append our new todo to the page
- 
-$taskName.append('<li class="tasklist">)' + toDoData.name + ' - ' + toDoData.description + '</li>');
-
-
+_.each(toDoItems, function (item, index) {
+  console.log(item);
+  var itemView = $(templatingFunction(item));
+  //$pet.attr('data-index', index);
+  tasklist.append(itemView);
+  console.log(itemView);
 });
 
+  newToDoList.on("submit", function(event) {
+    event.preventDefault();
+    toDoTask = $("#task-name").val();
+    toDoDesc = $("#task-desc").val();
+    console.log('form submitted!');
+    console.log(toDoTask + ' ' + toDoDesc);
+     
+ 
+tasklist.append('<li class="task">' + toDoTask + '  ' + toDoDesc + '</li>');
+
+   });
+});
+
+
 //add a click event handler to mark task done
- var $taskUL = $("#tasklist");
+ var $taskUL = $("#task-list");
 
 $taskUL.on("click", ".task", function(event) {
   console.log(this); 
